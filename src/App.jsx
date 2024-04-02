cfd9e04e1f90bd602b8530da227d2c2dd3be1762
Alex-fch <alexandre.foucher@live.fr>
2024-04-02 13:55:26 +0200

    create connexion page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (name.trim() === '') {
+      alert('Veuillez entrer un prénom.');
+      return;
+    }
+    setName('');
+    alert('Bonjour ' + name);
+  }
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  }
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous!</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Connectez-vous</h2>
+        <input type="text" value={name} placeholder="Entrez votre prénom..." onChange={handleChange} />
+        <button type="submit">Accédez à votre espace</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </form>
+  );
 }
 
-export default App
+export default App;
